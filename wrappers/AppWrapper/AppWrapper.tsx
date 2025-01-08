d2994e9c563150c2a31f472bc595a072cd4b35b6
Oleksandr Nikitin <oleksandr.nkitin@wetelo.com>
2025-01-08 15:57:42 +0200

    chore: use app color for status bar

--- wrappers/AppWrapper/AppWrapper.tsx
+++ wrappers/AppWrapper/AppWrapper.tsx
@@ -9,13 +9,18 @@ import { RootLoader, ScreenSuspense } from '@/managers';
 import { JotaiProvider } from '@/providers';
 import { FC, PropsWithChildren } from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { COLORS } from '@/constants';
 
 export const AppWrapper: FC<PropsWithChildren> = ({ children }) => {
   return (
     <JotaiProvider>
       <RootLoader>
         <ScreenSuspense>{children}</ScreenSuspense>
-        <StatusBar style="light" backgroundColor="black" translucent={false} />
+        <StatusBar
+          style="light"
+          backgroundColor={COLORS.background.app}
+          translucent={false}
+        />
       </RootLoader>
     </JotaiProvider>
   );
