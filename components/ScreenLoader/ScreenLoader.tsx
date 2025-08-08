bbcebf2dd52f091da9c992c97d303cb6c263c769
Vasyl Stasiuk <vasyl.stasiuk@wetelo.com>
2025-08-08 15:46:39 +0300

    fix: fixed position of screen loader

--- components/ScreenLoader/ScreenLoader.tsx
+++ components/ScreenLoader/ScreenLoader.tsx
@@ -1,15 +1,30 @@
 'use dom';
 
 import '@tailwind';
-
 import { Loader } from '../Loader';
+import { StyleSheet } from 'react-native';
 
 const ScreenLoader = () => {
   return (
-    <div className="h-full w-screen">
+    <div
+      className="h-screen w-screen flex items-center justify-center bg-white dark:bg-gray-900"
+      style={styles.container}
+    >
       <Loader />
     </div>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: 9999,
+    elevation: 9999,
+  },
+});
 
 export default ScreenLoader;
